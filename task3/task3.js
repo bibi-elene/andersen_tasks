1e0f070cdd7fff82756c64d285d698c98e774721
Elene B <eleneb@Elenes-MacBook-Air.local>
2023-08-08 23:12:41 +0400

    task 3.2

--- task3/task3.js
+++ task3/task3.js
@@ -5,15 +5,37 @@ Array.prototype.myFilter = function myFilter(callback, context) {
 
     const filtered = [];
 
-     for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         if (callback.call(context, this[i], i, this)) {
             filtered.push(this[i]);
         } 
-     }
+    }
+
      return filtered;
- }
+ };
 
 const arr = [1, 2, 3, 4, 5, 6];
 
 
-console.log(arr.myFilter((x, index) => {return index % 2 === 0}))
+arr.myFilter((x, index) => {
+    return index % 2 === 0;
+});
+
+function createDebounceFunction(callback, delay) {
+    let timeoutId;
+
+    return function() {
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            callback.apply(this, arguments);
+        }, delay);
+    };
+}
+
+
+const log100 = () => console.log(100);
+const debounceLog100 = createDebounceFunction(log100, 600);
+debounceLog100();
+setTimeout(debounceLog100, 200);
+setTimeout(debounceLog100, 400);
