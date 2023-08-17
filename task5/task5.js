ea0068d708bf844119b43573d0e1ad4fd67397e7
Elene B <eleneb@Elenes-MacBook-Air.local>
2023-08-17 23:31:47 +0400

    starting task5

---
+++ task5/task5.js
@@ -0,0 +1,21 @@
+
+class Stack {
+    constructor(maxSize) {
+        const res = [];
+
+        return res;
+
+    }
+
+    push = (el) => {return res + el };
+    pop = () => { }
+
+}
+
+const stackArr = new Stack(5,2,5,8);
+
+stackArr.push(87)
+stackArr.push(87)
+stackArr.push(827)
+
+console.log(stackArr)
