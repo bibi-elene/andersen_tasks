b060067058cbafe9be8a897d665b3ed835e6e3db
Elene B <eleneb@Elenes-MacBook-Air.local>
2023-08-17 18:02:53 +0400

    task 4.2

--- task4/task4.js
+++ task4/task4.js
@@ -3,20 +3,56 @@ function concatStrings(arg, separator) {
 
     return function(...innerArgs) {
         const isValidArg = innerArgs.length > 0 && innerArgs[0] !== undefined && innerArgs[0] !== null && typeof innerArgs[0] === 'string';
+        const concatStr = (separator !== undefined && separator !== null ? separator : '') + innerArgs[0];
 
         if (isValidArg) {
-            result += (separator !== undefined ? separator : '') + innerArgs[0];
+            result += concatStr;
             return concatStrings(result, separator);
         } else {
             return result;
         }
     };
-}
+};
 
+class Calculator {
+    constructor(x, y) {
+        if (!this.isValidNumber(x) || !this.isValidNumber(y)){
+            throw new Error('Invalid Number')
+        }
 
-console.log(concatStrings('first')('second')('third')());
-console.log(concatStrings('first', null)('second')());
-console.log(concatStrings('first', '123')('second')('third')());
-console.log(concatStrings('some-value')('333')(123n));
-console.log(concatStrings('some-value')(2));
-console.log(concatStrings('some-value')('')('')(null));
+        this.x = x;
+        this.y = y;
+    }
+
+    isValidNumber(num) {
+        return typeof num === 'number' && isFinite(num) && !isNaN(num);
+    }
+
+    setX = (num) => {
+        if (!this.isValidNumber(num)) {
+          throw new Error('Invalid Number');
+        }
+        return this.x = num;
+      }
+    
+    setY = (num) => {
+        if (!this.isValidNumber(num)) {
+            throw new Error('Invalid Number');
+        }
+        return this.y = num;
+    }
+
+    logSum = () => this.x + this.y;
+
+    logMul = () => this.x * this.y;
+
+    logSub = () => this.x - this.y;
+
+    logDiv = () => {
+        if (this.y === 0) {
+            throw new Error('Cannot divide by 0');
+        }
+    return this.x / this.y;
+    }
+
+};
